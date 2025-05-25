6ba763bdb187166a4a66fa4d537063875459c879
liangchow <liangchern@gmail.com>
2025-05-25 09:30:42 -0700

    Update Calendar.js
    Added const

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 export default function Calendar() {
+
+    const months = {'January': 'Jan', 'February': 'Feb', 'March': 'Mar', 'April': 'Apr', 'June': 'Jun', 
+      'July': 'Jul', 'August': 'Aug', 'September': 'Sept', 'October': 'Oct', 'November': 'Nov', 'December': 'Dec'}
+    const now = new Date()
+    const dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
   return (
-    <div>Calendar</div>
+    <div>
+      
+    </div>
   )
 }
